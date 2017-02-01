6c7e90f81a730fabee10048c74ba39a54cb341ce
Philip Hampton <plhampton@gmail.com>
2017-02-01 17:31:48 -0500

    gunicorn

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "Welcome to the Reserve DevOps portal"
+  return "Welcome to the Handy interview"
 
 if __name__ == '__main__':
   app.run()
